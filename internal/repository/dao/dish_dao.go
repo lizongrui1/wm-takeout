a60214730a03a268fb408a00fcbb196abd76df92
lizongrui_ <82227491+lizongrui1@users.noreply.github.com>
2024-04-18 23:09:51 +0800

    no message

--- internal/repository/dao/dish_dao.go
+++ internal/repository/dao/dish_dao.go
@@ -1,11 +1,22 @@
 package dao
 
-import "gorm.io/gorm"
+import (
+	"context"
+	"gorm.io/gorm"
+)
 
 type DishDao struct {
 	db *gorm.DB
 }
 
-func (d *DishDao) InsertDish() {
+func (d *DishDao) Transaction(ctx context.Context) (*gorm.DB, error) {
+	tx := d.db.WithContext(ctx).Begin()
+	if tx.Error != nil {
+		return nil, tx.Error
+	}
+	return tx, nil
+}
 
+func (d *DishDao) InsertDish() {
+	err := transaction
 }
--- internal/repository/dish_repo.go
+++ internal/repository/dish_repo.go
@@ -7,6 +7,7 @@ import (
 )
 
 type DishRepo interface {
+	Transaction(ctx context.Context) (*gorm.DB, error)
 	InsertDish(db *gorm.DB, dish *model.Dish) error
 	Delete(db *gorm.DB, id uint64) error
 	Update(db *gorm.DB, dish model.Dish) error
