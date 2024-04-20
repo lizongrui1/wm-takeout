8f56d7b6146d07ab3deeb67755644e07f8aaaf75
lizongrui_ <82227491+lizongrui1@users.noreply.github.com>
2024-04-20 13:27:04 +0800

    no message

--- internal/repository/dao/dish_dao.go
+++ internal/repository/dao/dish_dao.go
@@ -3,6 +3,7 @@ package dao
 import (
 	"context"
 	"gorm.io/gorm"
+	"wm-take-out/internal/model"
 )
 
 type DishDao struct {
@@ -17,6 +18,31 @@ func (d *DishDao) Transaction(ctx context.Context) (*gorm.DB, error) {
 	return tx, nil
 }
 
-func (d *DishDao) InsertDish() {
-	err := transaction
+func (d *DishDao) Commit(tx *gorm.DB) error {
+	return tx.Commit().Error
+}
+
+func (d *DishDao) RollBack(tx *gorm.DB) error {
+	return tx.Rollback().Error
+}
+
+func (d *DishDao) InsertDish(tx *gorm.DB, dish *model.Dish) error {
+	err := tx.Create(dish).Error
+	return err
+}
+
+func (d *DishDao) Delete(db *gorm.DB, id uint64) error {
+	err := db.Delete(&model.Dish{
+		Id: id,
+	}).Error
+	return err
+}
+
+func (d *DishDao) Update(db *gorm.DB, dish model.Dish) error {
+	err := db.Model(&dish).Updates(dish).Error
+	return err
+}
+
+func (d *DishDao) GetById(db *gorm.DB, id uint64) (*model.Dish, error) {
+
 }
--- internal/repository/dish_repo.go
+++ internal/repository/dish_repo.go
@@ -8,6 +8,8 @@ import (
 
 type DishRepo interface {
 	Transaction(ctx context.Context) (*gorm.DB, error)
+	Commit(tx *gorm.DB) error
+	RollBack(tx *gorm.DB) error
 	InsertDish(db *gorm.DB, dish *model.Dish) error
 	Delete(db *gorm.DB, id uint64) error
 	Update(db *gorm.DB, dish model.Dish) error
