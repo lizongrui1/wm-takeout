fe7676bb1cea99d077524093e8fb6175465c41da
lizongrui_ <82227491+lizongrui1@users.noreply.github.com>
2024-04-30 22:31:01 +0800

    no message

--- internal/service/dish_service.go
+++ internal/service/dish_service.go
@@ -2,9 +2,12 @@ package service
 
 import (
 	"context"
+	"strconv"
 	"wm-take-out/common"
 	"wm-take-out/internal/api/request"
 	"wm-take-out/internal/api/response"
+	"wm-take-out/internal/model"
+	"wm-take-out/internal/repository"
 )
 
 type DishService interface {
@@ -15,4 +18,29 @@ type DishService interface {
 	PageQuery(ctx context.Context, dto request.DishPageQueryDTO) (*common.PageResult, error)
 	SetStatus(ctx context.Context, id uint64, status int) error
 	List(ctx context.Context, id uint64) ([]response.DishListVo, error)
+}
+
+type DishSe struct {
+	repo           repository.DishRepo
+	dishFlavorRepo repository.DishFlavorRepo
+}
+
+func (ds *DishSe) InsertDish(ctx context.Context, dto request.DishDTO) error {
+	price, _ := strconv.ParseFloat(dto.Price, 10)
+	dish := model.Dish{
+		Id:   0,
+		Name: dto.Name,
+	}
+}
+
+func (ds *DishSe) DeleteDish(ctx context.Context, id string) error {
+
+}
+
+func (ds *DishSe) UpdateDish(ctx context.Context, dto request.DishUpdateDTO) error {
+
+}
+
+func (ds *DishSe) GetDishById(ctx context.Context, id uint64) (response.DishVo, error) {
+
 }
