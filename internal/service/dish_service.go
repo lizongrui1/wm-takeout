25c8e8d46a62ee1326f4c017871342670af48969
zongrui li <192292177@qq.com>
2024-05-06 08:46:06 +0800

    no message

--- internal/service/dish_service.go
+++ internal/service/dish_service.go
@@ -18,7 +18,7 @@ type DishService interface {
 	DeleteDish(ctx context.Context, id string) error
 	UpdateDish(ctx context.Context, dto request.DishUpdateDTO) error
 	GetDishById(ctx context.Context, id uint64) (response.DishVo, error)
-	PageQuery(ctx context.Context, dto request.DishPageQueryDTO) (*common.PageResult, error)
+	PageQuery(ctx context.Context, dto *request.DishPageQueryDTO) (*common.PageResult, error)
 	SetStatus(ctx context.Context, id uint64, status int) error
 	List(ctx context.Context, id uint64) ([]response.DishListVo, error)
 }
@@ -183,4 +183,8 @@ func (ds *DishSe) List(ctx context.Context, id uint64) ([]response.DishListVo, e
 		})
 	}
 	return dishListVo, nil
+}
+
+func NewDishSe(repo repository.DishRepo, flavorRepo repository.DishFlavorRepo) DishService {
+	return &DishSe{repo: repo, dishFlavorRepo: flavorRepo}
 }
